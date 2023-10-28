c7458e05775ada59211543ea115407df75e3dfd8
Devin Dow <DevinDow@gmail.com>
2023-10-28 14:59:47 -0700

    Othello with Animation

--- othello/flutter_service_worker.js
+++ othello/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "53356af85aa285daca6417c4c436ff6a",
-"/": "53356af85aa285daca6417c4c436ff6a",
-"main.dart.js": "46f4d153be9f5912f09dff6c3b574d82",
+"index.html": "9f9bbaab3ff89f73941ba4f653fa7de7",
+"/": "9f9bbaab3ff89f73941ba4f653fa7de7",
+"main.dart.js": "4f30540435230f630b6f5f541467e867",
 "manifest.json": "956b511e0799355b28e11e4fc5c836cb",
 "version.json": "7e7eb9e1fbb72eaa276809b14b700692"};
 // The application shell files that are downloaded before a service worker can
